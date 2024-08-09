e51545b436f28454c33f6861514c84617ca6e02e
VishalValvi-83 <vishalvalvi2529@gmail.com>
2024-08-09 21:46:29 +0530

    animation to banner content

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,10 +4,10 @@ function Banner({ handleSearch, searchTerm }) {
     return (
         <div className="home-banner">
             <div className="banner-content">
-                <h1 className="site-title">MovieHouse</h1>
-                <p>Discover the latest movies and TV shows</p>
+                <h1 className="site-title animate__animated animate__fadeInDown ">MovieHouse</h1>
+                <p className='animate__animated animate__fadeInLeft'>Discover the latest movies and TV shows</p>
                 <div className="search sm:4/5 md:w-2/5">
-                    <input type="text" className="search-input w-full" placeholder="Search for movies or TV shows" value={searchTerm} onChange={handleSearch} />
+                    <input type="text" className="search-input animate__fadeInRight animate__animated w-full" placeholder="Search for movies or TV shows" value={searchTerm} onChange={handleSearch} />
                     {/* <button type="submit" className="search-btn">Search</button> */}
                 </div>
             </div>
